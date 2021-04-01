84584d45d4900c6d059b857afb4f08b1994a5a19
wuguanghong98 <wuguho@163.com>
2021-04-01 20:22:21 +0800

    '引入并使用轮播图组件'

--- src/views/home/child/HomeBanner.vue
+++ src/views/home/child/HomeBanner.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="Banner">
-    <div v-for="banner in banners">{{banner.title}}</div>
-  </div>
+    <swiper>
+      <swiper-item v-for="banner in banners">
+          <a :href="banner.link">
+            <img :src="banner.image" alt="">
+          </a>
+      </swiper-item>
+    </swiper>
+    </div>
 </template>
 
 <script>
 
+  import {Swiper,SwiperItem} from 'components/common/swiper/index'
+
   import * as req from 'network/requestMethods'
   export default {
     name: "HomeBanner",
+    components: {
+      Swiper,
+      SwiperItem
+    },
     data(){
       return {
         banners: null,
