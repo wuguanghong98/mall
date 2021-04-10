7e5a0bd8a50dadd37147fb0645c87e4b6e1f2cfe
wuguanghong98 <wuguho@163.com>
2021-04-10 22:25:41 +0800

    '修复详情中betterScroll的偶现无法翻滚bug'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -11,36 +11,6 @@
       <detail-goods-param :tableParams="tableParams" :paramInfoSet="paramInfoSet"/>
       <detail-goods-rate :user="user" :rateInfo="rateInfo"/>
       <detail-recommend/>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>11</li>
-        <li>12</li>
-        <li>13</li>
-        <li>14</li>
-        <li>15</li>
-        <li>16</li>
-        <li>17</li>
-        <li>18</li>
-        <li>19</li>
-        <li>20</li>
-        <li>21</li>
-        <li>22</li>
-        <li>23</li>
-        <li>24</li>
-        <li>25</li>
-        <li>26</li>
-        <li>27</li>
-
-      </ul>
     </scroll>
     iid:{{$route.query.iid}}
   </div>
@@ -103,6 +73,10 @@
         this.paramInfoSet = data.itemParams.info.set
         this.user = data.rate.list[0].user
         this.rateInfo = data.rate.list[0]
+      })
+
+      this.$bus.$on('goodsImgLoad', () => {
+        this.$refs.scroll.refresh()
       })
     },
     methods: {
